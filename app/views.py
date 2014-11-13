bb2a5e4204a9bc8c14d28cc3aba78aae1e72cbb9
Jett <whackashoe@gmail.com>
2014-11-12 20:37:31 -0600

    catch bad parses

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from flask import render_template, request
 from app import app
-from pandas import read_csv
+from pandas import read_csv, parser
 
 
 @app.route('/')
@@ -13,7 +13,11 @@ def upload():
     up_file = request.files['file']
 
     if up_file:
-        data = read_csv(up_file)
+    	try:
+        	data = read_csv(up_file)
+        except (parser.CParserError):
+      		return render_template('upload_error.html', errors=["CSV parse error"])
+
         return render_template('render.html', table=data.to_html())
     else:
         return render_template('upload_error.html', errors=["no file uploaded"])
